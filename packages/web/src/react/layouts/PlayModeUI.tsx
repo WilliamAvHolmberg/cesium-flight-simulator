640f6ddf0fb72dd84fbfda7bda7edadc64d7aa55
William Holmberg <william.av.holmberg@gmail.com>
2025-10-28 06:50:55 +0100

    refactor: Remove LocationSelector from PlayModeUI component

--- packages/web/src/react/layouts/PlayModeUI.tsx
+++ packages/web/src/react/layouts/PlayModeUI.tsx
@@ -1,7 +1,6 @@
 import { HUD } from '../features/hud/components/HUD';
 import { ControlsPanel } from '../features/controls/components/ControlsPanel';
 import { CameraControls } from '../features/camera/components/CameraControls';
-import { LocationSelector } from '../features/location/components/LocationSelector';
 import { CrashScreen } from '../features/crash/components/CrashScreen';
 import { MiniMap } from '../features/minimap/components/MiniMap';
 
@@ -10,7 +9,6 @@ export function PlayModeUI() {
     <>
       <ControlsPanel />
       <div className="fixed top-8 right-8 z-50 flex gap-2 pointer-events-auto">
-        <LocationSelector />
         <CameraControls />
       </div>
       <HUD />
